6a8186c18b7b0648c4b879895209b9c334805d32
Gideon Lyomu <65831672+lyomu@users.noreply.github.com>
2023-05-11 20:41:13 +0300

    Header section complete

---
+++ 10_js_projects/quiz_app/script.js
@@ -0,0 +1,40 @@
+const quizData= [
+    {
+        question: 'How Old is Captain America',
+        a: '10',
+        b: '150',
+        c: '26',
+        d: '30',
+        correct: 'c',
+    },{
+        question: 'What is the most used programming Language in 2023?',
+        a: 'Java',
+        b: 'Python',
+        c: 'C',
+        d: 'Javascript',
+        correct: 'd',
+    },{
+        question: 'What is the most commonly used IDE?',
+        a: 'VS Code',
+        b: 'Sublime',
+        c: 'NotePad',
+        d: 'Android Studio',
+        correct: 'a',
+    },{
+        question: 'What does HTML stand For?',
+        a: 'Hypertext Markup Language',
+        b: "Cascading Style Sheet",
+        c: "Javascript Object Notation",
+        d: 'Helicopter Terminals Motorboats Lamboghini',
+        correct: 'a',
+    },{
+        question: 'When was Javascript Launched',
+        a: '1996',
+        b: "1995",
+        c: "1994",
+        d: 'none of the above',
+        correct: 'd',
+    }
+
+
+]
